58d6010e3b766f8cc4810d2a23fb61a4bfe5b260
Migara Denuwan <migaradenuwan99999999@gmail.com>
2024-05-10 22:17:28 +0530

    create edit and delete for the crud

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.todoapp;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,40 +15,57 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todoapp.Adapter.ToDoAdapter;
 import com.example.todoapp.Model.ToDoModel;
+import com.example.todoapp.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogCloseListener{
 
     private RecyclerView taskRecyclerView;
     private ToDoAdapter taskAdapter;
+    private FloatingActionButton fab;
 
     private List<ToDoModel> taskList;
+    private DatabaseHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
         taskList = new ArrayList<>();
 
         taskRecyclerView = findViewById(R.id.tasksRecyclerView);
         taskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        taskAdapter = new ToDoAdapter(this);
+        taskAdapter = new ToDoAdapter(db,this);
         taskRecyclerView.setAdapter(taskAdapter);
 
-        ToDoModel task = new ToDoModel();
-        task.setTask("This is a Test Task");
-        task.setStatus(0);
-        task.setId(1);
+        fab = findViewById(R.id.fab);
 
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-        taskList.add(task);
-
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
         taskAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(),AddNewTask.TAG);
+            }
+        });
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        taskAdapter.setTasks(taskList);
+        taskAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/todoapp/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/todoapp/Utils/DatabaseHandler.java
@@ -24,7 +24,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             + STATUS + " INTEGER)";
     private SQLiteDatabase db;
 
-    private DatabaseHandler(Context context){
+    public DatabaseHandler(Context context){
         super(context, NAME, null, VERSION);
     }
     @Override
